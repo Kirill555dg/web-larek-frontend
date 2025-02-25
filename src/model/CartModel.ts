6536c9ff0767a780d6f835c54cc61146058b0b95
Kerrodar <Kirill555dg@gmail.com>
2025-02-25 23:52:42 +0300

    feat(model): implement base models

---
+++ src/model/CartModel.ts
@@ -0,0 +1,29 @@
+import { EventEmitter } from '../core/events/EventEmitter';
+
+export default class CartModel {
+	private _items: string[] = [];
+
+	constructor(private emitter: EventEmitter) {
+		// TODO: Восстановление из LocalStorage
+	}
+
+	addItem(productId: string): void {
+		if (!this._items.includes(productId)) {
+			this._items.push(productId);
+			this.emitter.emit('cart:updated', this._items);
+			// TODO: Сохранение в LocalStorage
+		}
+	}
+
+	removeItem(productId: string): void {
+		this._items = this._items.filter((id) => id !== productId);
+		this.emitter.emit('cart:updated', this._items);
+		// TODO: Обновление LocalStorage
+	}
+
+	get items(): string[] {
+		return [...this._items];
+	}
+
+	// TODO: Добавить метод расчета суммы
+}
