c1c9cddf4aeceec493682adc8ab815045b0ebf4f
Kerrodar <Kirill555dg@gmail.com>
2025-03-25 09:36:30 +0300

    refactor: move all components into new folder

---
+++ src/components/model/OrderModel.ts
@@ -0,0 +1,20 @@
+import OrderApi from '../api/OrderApi';
+import { EventEmitter } from '../core/EventEmitter';
+import { Order } from '../../types';
+
+export default class OrderModel {
+    constructor(private api: OrderApi, private emitter: EventEmitter) { }
+
+    async submitOrder(order: Order): Promise<void> {
+        try {
+            // TODO: Добавить валидацию данных
+            const result = await this.api.submitOrder(order);
+            this.emitter.emit('order:success', result);
+        } catch (error) {
+            // TODO: Типизировать ошибки
+            this.emitter.emit('order:error', error);
+        }
+    }
+
+    // TODO: Добавить методы валидации полей
+}
